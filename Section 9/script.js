00bd6f7f4ea518182d03f6db7ec68eea9fefa905
huayuehu <huayuehu@usc.edu>
2020-07-21 16:15:59 -0700

    New change

--- Section 9/script.js
+++ Section 9/script.js
@@ -1,5 +1,19 @@
-var userFirstName = prompt("What is your first name?");
-var userLastName = prompt("What is your last name?");
-console.log("Your full name is " + userFirstName + " " + userLastName);
-var userAge = prompt("How old are you honey?");
-console.log("You are " + userAge + " years old!");
+// var userFirstName = prompt("What is your first name?");
+// var userLastName = prompt("What is your last name?");
+// console.log("Your full name is " + userFirstName + " " + userLastName);
+// var userAge = prompt("How old are you honey?");
+// console.log("You are " + userAge + " years old!");
+
+var age = prompt("How old are you honey?");
+if (age < 0) {
+    console.log("ERROR!!!!!!!");
+} 
+if (age == 21) {
+    console.log("Happy 21st birthday!!");
+}
+if ((age % 2) == 1) {
+    console.log("Your age is odd!");
+}
+if (age % Math.sqrt(age) == 0) {
+    console.log("Your age is a perfect square!");
+}
