08abba2bd36f2fe3e059b75c7eb0ad93ad4bc476
huayuehu <huayuehu@usc.edu>
2020-07-21 15:49:39 -0700

    "New learning"

---
+++ Section 9/script.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+console.log("Your full name is " + userFirstName + " " + userLastName);
+var userAge = prompt("How old are you honey?");
+console.log("You are " + userAge + " years old!");
